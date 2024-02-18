1592d658f68f01983db9a69ceb51046e979138b5
Alan Francis <alanfrancisangra@gmail.com>
2024-02-17 23:34:36 -0300

    Ajustes

--- pichau_price.py
+++ pichau_price.py
@@ -1,9 +1,9 @@
 from playwright.sync_api import sync_playwright
+import re
 
 # instalar
 # pip install playwright
 # playwright install
-
 
 def fetch_product_details(url):
     with sync_playwright() as p:
@@ -17,14 +17,16 @@ def fetch_product_details(url):
         # Seleciona todos os cartões de produtos na página
         cards = page.query_selector_all('div.MuiCardContent-root')
 
+        products = []
         for card in cards:
             # Captura a descrição dentro do <h2> para cada produto
             descricao = card.query_selector('h2').inner_text()
             # Captura o preço. Note que você pode precisar ajustar o seletor dependendo da estrutura exata da página
-            preco = card.query_selector(
-                'div[class*="jss"]:has-text("R$")').inner_text()
-            print(f'Descrição: {descricao}, Preço: {preco}')
+            preco = card.query_selector('div[class*="jss"]:has-text("R$")').inner_text()
+            preco = re.search(r'à vista\nR\$\s*([\d.,]+)', preco).group(1).replace('.', '').replace(',', '.')
+            products.append({"name": descricao, "price": preco})
         browser.close()
+        return products
 
-
-fetch_product_details("https://www.pichau.com.br/hardware/placa-de-video")
+dados = fetch_product_details("https://www.pichau.com.br/hardware/placa-de-video")
+print(dados)
