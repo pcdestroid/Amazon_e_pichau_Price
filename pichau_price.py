262b30a61bc4c4f107c4084b96a0cc9b3377ced5
Alan Francis <alanfrancisangra@gmail.com>
2024-02-18 00:21:42 -0300

    mais ajustes

--- pichau_price.py
+++ pichau_price.py
@@ -5,28 +5,46 @@ import re
 # pip install playwright
 # playwright install
 
+# Documentação
+# https://playwright.dev/python/docs/intro
+
+
 def fetch_product_details(url):
     with sync_playwright() as p:
-        # Use headless=True em ambiente de produção
-        # headless=True remove a visão do navegador
+
         browser = p.chromium.launch(headless=False)
         page = browser.new_page()
         page.goto(url)
-        # Espera pelo carregamento de um elemento que contém a descrição e o preço
+
         page.wait_for_selector('div.MuiCardContent-root')
-        # Seleciona todos os cartões de produtos na página
         cards = page.query_selector_all('div.MuiCardContent-root')
 
         products = []
         for card in cards:
-            # Captura a descrição dentro do <h2> para cada produto
-            descricao = card.query_selector('h2').inner_text()
-            # Captura o preço. Note que você pode precisar ajustar o seletor dependendo da estrutura exata da página
-            preco = card.query_selector('div[class*="jss"]:has-text("R$")').inner_text()
-            preco = re.search(r'à vista\nR\$\s*([\d.,]+)', preco).group(1).replace('.', '').replace(',', '.')
+
+            descricao = card.query_selector(
+                'h2').inner_text().lower().replace("placa de video", "")
+            descricao = descricao.replace("\n", " ").replace(
+                ",", "").replace("  ", " ")
+
+            match = re.search(r'.*?gb', descricao)
+            # match = re.search(r'.{10}(?=GDDR)', descricao)
+            if match:
+                descricao = match.group()
+
+            preco = card.query_selector(
+                'div[class*="jss"]:has-text("R$")').inner_text()
+            preco = re.search(
+                r'à vista\nR\$\s*([\d.,]+)', preco).group(1).replace(".", "").replace(",", ".")
+
             products.append({"name": descricao, "price": preco})
         browser.close()
         return products
 
-dados = fetch_product_details("https://www.pichau.com.br/hardware/placa-de-video")
+
+# dados = fetch_product_details("https://www.pichau.com.br/search?q=GTX1660")
+dados = fetch_product_details(
+    "https://www.pichau.com.br/hardware/placa-de-video")
 print(dados)
+# print(dados[0])
+# print(dados[1])
